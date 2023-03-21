cfc62ea33c4081ad35915868c60b3b772102297c
Nataliia Odai <nataliiaodai22@gmail.com>
2023-03-21 21:16:26 +0200

    header & footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from "./product-list/product-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'client/catalog', component: ProductListComponent},
+  // {path: 'client/catalog/detail:productId', component: ProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
