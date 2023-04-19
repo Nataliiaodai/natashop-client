63a276a28a9cb076828e1eeae041d31327fbf246
Nataliia Odai <nataliiaodai22@gmail.com>
2023-04-19 18:43:37 +0300

    tmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {CategoryTreeModel} from "./shared-model/category-tree.model";
+import {CategoryService} from "./category/category.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-client-proj';
+  categoryMenu: boolean = false;
+  categoryTree: CategoryTreeModel = new CategoryTreeModel();
+
+  constructor(public router: Router,
+              private categoryService: CategoryService) {
+  }
 
 
+  ngOnInit() {
+    this.fetchCategoryTree();
+  }
+
+
+  fetchCategoryTree() {
+    this.categoryService.fetchCategoryList()
+      .subscribe((categoryTreeResponse) => {
+        console.log(categoryTreeResponse);
+        this.categoryTree = categoryTreeResponse;
+
+      })
+  };
+
+  onGetCategoryDetail(categorySlug: string) {
+    console.log(categorySlug);
+    this.categoryMenu = !this.categoryMenu;
+    this.router.navigate([`categories/${categorySlug}`])
+      .then();
+  }
 
 }
